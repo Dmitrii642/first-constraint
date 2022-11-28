f9457ae3623a5829f4d6f514fb6ae00dd9df1817
firstApp <firstApp>
2022-11-28 17:40:11 +0300

    Initial Commit

---
+++ MFA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MFA
+//
+//  Created by Dmitrii Iakovlev on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+
+
+}
+
